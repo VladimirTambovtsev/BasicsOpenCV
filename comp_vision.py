6cc25930a473baaa49a8dd908dfc52bfb3eef44f
Vladimir Tambovtsev <tambovcev99@mail.ru>
2019-08-10 15:01:45 +0300

    set up smile detection in comp_vision

--- comp_vision.py
+++ comp_vision.py
@@ -3,19 +3,24 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('faces/haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('faces/haarcascades/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('faces/haarcascades/haarcascade_smile.xml')
 
 
 def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x: x + w]
-        roi_color = frame[y: y + h, x: x + w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
 
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
     return frame
 
 
